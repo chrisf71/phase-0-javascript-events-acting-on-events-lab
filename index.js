4d2e33e2deacb6dcea967bc7e70bf97811ac6a38
chrisf71 <christopherf442@hotmail.com>
2023-12-31 23:39:32 +0000

    complet events acting on events

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+// Grabbing the dodger element
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+// Function to handle moving the dodger/box to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left
+            + 1}px`;
+    }
+}
+
+// Event listener to handle keydown events or holding a key
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
